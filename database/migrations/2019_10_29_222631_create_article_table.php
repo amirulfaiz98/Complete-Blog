b7afda5aa8299766b010683b4d935e3ea69b752b
Amirul Faiz <amirulfaiz.work@gmail.com>
2019-10-30 21:18:36 +0800

    Add Route - Hello World

--- database/migrations/2019_10_29_222631_create_article_table.php
+++ database/migrations/2019_10_29_222631_create_article_table.php
@@ -15,6 +15,9 @@ class CreateArticleTable extends Migration
     {
         Schema::create('article', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title'); //attribute title
+            $table->text('body'); //text usually for long words
+            $table->boolean('published')->default(0); // checking whether the article has published or not
             $table->timestamps();
         });
     }
