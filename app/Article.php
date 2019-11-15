d21399687f0c77f20724e1162d9d0919b5c374d6
Amirul Faiz <amirulfaiz.work@gmail.com>
2019-11-16 00:22:18 +0800

    Define 1:M relationship

--- app/Article.php
+++ app/Article.php
@@ -9,4 +9,8 @@ class Article extends Model
     protected $table = 'article';
 
     protected $fillable = ['title', 'body', 'published'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
